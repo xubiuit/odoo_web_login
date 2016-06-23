dd25ff6cefa49d976205e3b156d91b4056e2df60
Binh Nguyen Xuan <kid.uit.1412@gmail.com>
2016-06-23 21:56:21 +0700

    [UPD] Change license

--- odoo_web_login/__openerp__.py
+++ odoo_web_login/__openerp__.py
@@ -5,7 +5,18 @@
 #    Copyright (C) 2016- XUBI.ME (http://www.xubi.me)
 #    @author binhnguyenxuan (https://www.linkedin.com/in/binh-nguyen-xuan-46556279)
 #
-#    This program is paid software base on OEEL-1 license
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    
 #    Background Source: http://forum.xda-developers.com/showpost.php?p=37322378
 #
@@ -18,9 +29,9 @@
     'summary': """
 The new configurable Odoo Web Login Screen
 """,
-    'author': "binhnguyenxuan (https://www.linkedin.com/in/binh-nguyen-xuan-46556279)",
+    'author': "binhnguyenxuan (www.xubi.me)",
     'website': 'http://www.xubi.me',
-    'license': 'OEEL-1',
+    'license': 'AGPL-3',
     'depends': [
     ],
     'data': [
@@ -31,6 +42,4 @@ The new configurable Odoo Web Login Screen
     ],
     'installable': True,
     'application': True,
-    'price': 11.00,
-    'currency': 'EUR',
 }
